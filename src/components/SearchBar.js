aba5c9218179246029a77951a83a90065111dce9
Richard Hoffmann <r.hoffmann@crolla-lowis.de>
2016-08-15 00:30:53 +0200

    controlled component Searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const SearchBar = () => <div>SearchBar</div>;
+class SearchBar extends Component {
+
+  static propTypes: {
+    placeholder: PropTypes.string
+  }
+
+  constructor(props) {
+    super(props);
+
+    // getInitialState
+    this.state = {
+      term: ''
+    };
+  }
+
+  render() {
+    return (
+      <div ref='piff'>
+        <input
+          ref={component => this._input = component}
+          value={this.state.term}
+          onChange={() => this.setState({ term: this._input.value })}
+          className="form-control"
+          placeholder={this.props.placeholder}
+          type="text"
+        />
+        <h3>Term:{this.state.term}</h3>
+      </div>
+    );
+  }
+}
+
+SearchBar.defaultProps = {
+  placeholder: 'Search...'
+};
 
 export default SearchBar;
