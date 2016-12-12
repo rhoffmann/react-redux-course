1172e12bd79e7e4fce9c6974faecc8f83d1bfbe6
Richard Hoffmann <r.hoffmann@crolla-lowis.de>
2016-12-12 17:34:59 +0100

    removed one reducer

--- book-list/src/reducers/books.js
+++ book-list/src/reducers/books.js
@@ -1,6 +1,6 @@
-import { Types } from '../actions';
-
 import { createReducer } from 'reduxsauce';
+
+import { Types } from '../actions';
 
 const INITIAL_STATE = [
   { id: 1, title: 'Javascript: The Good Parts', pages: 213 },
@@ -9,10 +9,10 @@ const INITIAL_STATE = [
   { id: 4, title: 'I dont know', pages: 1}
 ];
 
-const doNothing = (state = INITIAL_STATE, action) => state;
+// const doNothing = (state = INITIAL_STATE, action) => state;
 
 const ACTION_HANDLERS = {
-  [Types.SELECT_BOOK]: doNothing
+  // [Types.SELECT_BOOK]: doNothing
 };
 
 export default createReducer(INITIAL_STATE, ACTION_HANDLERS);
