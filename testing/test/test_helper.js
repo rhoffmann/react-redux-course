1b1f8870cf1900f680e263b3670c36151ae015b1
Richard Hoffmann <r.hoffmann@crolla-lowis.de>
2016-12-22 17:54:39 +0100

    cleanup

--- testing/test/test_helper.js
+++ testing/test/test_helper.js
@@ -1,37 +1,42 @@
-import _$ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
+import _$ from 'jquery';
 import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from '../src/modules/rootReducer';
-// import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
+
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+const $ = _$(global.window);
 
-chaiJquery(chai, chai.util, $);
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(rootReducer, state)}>
       <ComponentClass {...props} />
     </Provider>
   );
 
+  // produces HTML wrapped in jquery
   return $(ReactDOM.findDOMNode(componentInstance));
 }
+
 
 $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
   }
-  TestUtils.Simulate[eventName](this[0]);
+
+  TestUtils.Simulate[eventName](this.get(0), value);
 };
 
-export {renderComponent, expect};
+
+chaiJquery(chai, chai.util, $);
+
+
+export { renderComponent, expect };
